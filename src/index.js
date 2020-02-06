65c266ecbb4877a7d7293fa3ccd3120e7e00de48
Tanner Faber <tannerfaber@eduroam-169-233-253-18.ucsc.edu>
2020-02-05 16:57:01 -0800

    Make app deployable

--- src/index.js
+++ src/index.js
@@ -1,34 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./ArrayElement.js";
-import ArrayElement from "./ArrayElement.js";
-import "./style.css";
-
-class App extends React.Component {
-  state = { arrayMain: [] };
-
-  handleReset = () => {
-    console.log(this.state);
-    this.setState({ arrayMain: [] });
-  };
-
-  render() {
-    return (
-      <div className="container segment">
-        <div className="ui menu">
-          <div className="item">
-            <div className="ui primary button">Run</div>
-          </div>
-          <div className="item">
-            <button onClick={this.handleReset} className="ui button">
-              Reset
-            </button>
-          </div>
-        </div>
-        <ArrayElement array={this.state.arrayMain} />
-      </div>
-    );
-  }
-}
+import App from "./App";
 
 ReactDOM.render(<App />, document.querySelector("#root"));
